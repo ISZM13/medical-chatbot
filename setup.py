0dc5058e2a2dc1df59a92b310def17a3760f3112
Ishtveer Singh <98831292+ISZM13@users.noreply.github.com>
2024-11-20 02:02:14 +0530

    Added all the local files.
    Added all the base project made for submission in the first push.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ishtveer Singh Billing',
+    author_email= 'ishtveersinghbilling@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+
+
+system_prompt = (
+    "You are an assistant made by Ishtveer to answer medical any medical related queries."
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise. You are the best medical chatbot in the world."
+    "\n\n"
+    "{context}"
+)
